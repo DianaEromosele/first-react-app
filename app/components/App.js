241577490af12b1b656ccb324f7e1aa380ca970f
DianaEromosele <diana.ozemebhoya@gmail.com>
2017-01-29 23:00:44 -0500

    Initial commit message

---
+++ app/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+
+var DianaComponentClass = React.createClass({
+	render: function () {
+		return <h1> Hey Diana, good job getting your first React App up and running! </h1>
+	}
+	
+})
+
+
+
+module.exports = DianaComponentClass
